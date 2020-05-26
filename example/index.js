c8ae202977da8a82259c9c875b40d63d18104f8d
Jérémie Zarca <jeremie.zarca@gmail.com>
2020-05-26 19:06:20 +0200

    updated example

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const city = new Cityscoot();
 async function init() {
   const scooters = await city.getScooters(4);
   console.log(scooters);
-  const scooter = await city.getScooter(5193);
+  const scooter = await city.getScooter(4, 5193);
   console.log(scooter);
 }
 
